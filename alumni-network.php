651367d1d38bc98b5a1670b35f01cf4bb2797ce7
Karaj Singh <karajsingh368@gmail.com>
2025-07-17 18:09:49 +0530

    add more pages and Content

---
+++ alumni-network.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html lang="zxx">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Alumni Network | Shobhit University</title>
+ 
+
+   <?php include 'header.php';?>
+    <!-- Start Section Banner Area -->
+    <section class="section-banner bg-1">
+        <div class="container">
+            <div class="banner-spacing">
+                <div class="section-info">
+                    <h2 data-aos="fade-up" data-aos-delay="100"> Alumni Network </h2>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Section Banner Area -->
+
+    <!-- Start About Us Area -->
+    <section class="academics-section ptb-100">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-lg-9">
+                    <div class="ac-overview">
+                        <div class="pera-dec">
+                        <p>Welcome to the Shobhit University / NICE Society Institutions Alumni Network. No matter how many years, months or even weeks it has been since you were a student, this is your chance to reconnect with other former friends and get reacquainted with your Alma matte. We owe our success to our students – present and past – and appreciate all that you contributed as a former member of our team. That's why we've created this network as a valuable resource for you.</p>
+                        <p>As a member, you will have the opportunity to renew old friendships, establish new ones, expand your professional network, and have access to events, news, and exciting career prospects. We hope this is the continuation of a lifelong relationship with you. We appreciate the many contributions that our alumni have made to University's growth and success. We created this network as a way for you to stay connected with us and the many friends that you made during your time here. This is your resource and we want you to take advantage of it.</p>
+                        <p>“You're selected." Remember that day? As an alumnus, if you'd like to experience that feeling again, we have an offer for you. At Shobhit University – Centre for Professional Development we’re interested in partnering with you on innovative ways to keep improving the professional lives of the existing Shobhitians.</p>
+                        <p>The Alumni Network is a nonprofit network developed by alumni to help former students stay connected. Our mission is to share ideas, talents and resources with each other and with our communities. We will donate any profits to charitable groups around the world. We are not formally connected with the University, but they are supportive of our efforts.</p>
+                        <p>Your affiliation with your alma-matte affords you many personal benefits, and as an alumnus you can also access valuable services offered exclusively to you by our affinity partners</p>
+                        <ul>
+                          <li><p>Stay with us: If you are travelling to a city where Shobhit University has its facility, you can stay free of cost at the campus.</p></li>
+                          <li><p>Use the Business Centre: If you are travelling to Delhi, you can use the Business Centre Facility at the Corporate Office of the organization at University Tower, Mayur Vihar Phase II, Delhi -91, free of cost to meet a business colleague or even just to check your mail.</p></li>
+                         
+                        </ul>
+                        <p>Please contact <a href="alumni@shobhituniversity.ac.in">alumni@shobhituniversity.ac.in</a>, in advance (preferably 10 working days) with your Name, Batch, College information to find availability and confirmation.</p>
+                        <p>There are lots of other alumni benefits which are currently under discussion. Please revisit this page later to know more about them. If you have any questions or suggestions please write to us at <a href="alumni@shobhituniversity.ac.in">alumni@shobhituniversity.ac.in</a>
+                          <br>May be you will love to see the photo gallery of some alumni reunions here and will be motivated to join the next one.</p>
+                        <ul>
+
+                      <li><strong><a href="https://www.shobhituniversity.ac.in/pdf/naac/alumni-registration-certificate.pdf" target="_blank">Alumni Registration Certificate</a></strong></li>
+                      <li><strong><a href="https://www.shobhituniversity.ac.in/pdf/naac/ALUMINI-MEET-AND-CHAPTERS-PDF.pdf" target="_blank">Alumni Meet and Chapters</a></strong></li>
+                      <li><strong><a href="https://www.shobhituniversity.ac.in/pdf/naac/Alumni-Contribution-2016-21.pdf" target="_blank">Alumni Contribution 2016-21</a></strong></li>
+
+                     
+
+                      </ul>
+                      </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="academics-left">
+                        <div class="ac-category">
+                         <div class="ac-contact">
+                            <span class="text-black">Get to Know Shobhit</span>
+                        </div>
+                        <ul>
+                            <li><a href="MOUs.php" title="MOU's">MOU's</a></li>
+                            <li><a  href="social-services.php" title="Social Services">Social
+                                Services</a></li>
+                            <li><a href="awards-and-achievements.php" title="Awards and Achievements">Awards and Achievements </a></li>
+                            <li><a class="active" href="alumni-network.php" title="Alumni Network">Alumni
+                                Network</a></li>
+                            <li><a href="notable-alumni.php" title="Notable Alumni">Notable
+                                Alumni</a></li>
+                            <li><a href="seminar-and-conferences.php" title="Seminar &amp; Conferences">Seminar &amp; Conferences </a></li>
+                            <li><a href="student-clubs.php" title="Notable Alumni">Student Clubs</a></li>
+                            <li><a href="library-and-learning-centre.php" title="Notable Alumni">Library &amp; Learning
+                                Centre</a></li>
+                          </ul>
+                        
+                        </div>
+                        <div class="ac-contact">
+                            <span class="text-black">Quick Links</span>
+                            <a href="https://admission.shobhituniversity.ac.in/" target="_blank">Apply Now</a>
+                            <a class="darkbtn" href="contact-us.php">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End About Us Area -->
+   
+
+    <!-- Campus Slider  -->
+    <section class="campus-area ptb-100">
+        <div class="container-fluaid">
+          <div class="section-title aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+            <div class="sub-title">
+              <i class="bx bxs-graduation"></i>
+              <p>The Campus Experience</p>
+            </div>
+            <h2 class="title-anim">One University, Many Places</h2>
+          </div>
+
+          <div class="campus-slider owl-carousel owl-theme owl-loaded owl-drag">
+            
+            
+            
+          <div class="owl-stage-outer owl-height" style="height: 487.75px;"><div class="owl-stage" style="transition: 1s; width: 6650px; transform: translate3d(-2375px, 0px, 0px);"><div class="owl-item cloned" style="width: 930px; margin-right: 20px;"><div class="campus-card aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+              <img src="assets/img/all-img/campus-image-2.png" alt="image">
+              <div class="content">
+                <h2 class="title-anim">Law School</h2>
+                <!-- <a class="default-btn" href="the-campus-experience.html">Virtual Tour</a> -->
+              </div>
+            </div></div><div class="owl-item cloned" style="width: 930px; margin-right: 20px;"><div class="campus-card aos-init aos-animate" data-aos="fade-up" data-aos-delay="300">
+              <img src="assets/img/all-img/campus-image-3.png" alt="image">
+              <div class="content">
+                <h2 class="title-anim">Humanities</h2>
+                <!-- <a class="default-btn" href="the-campus-experience.html">Virtual Tour</a> -->
+              </div>
+            </div></div><div class="owl-item" style="width: 930px; margin-right: 20px;"><div class="campus-card aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+              <img src="assets/img/all-img/campus-image-1.png" alt="image">
+              <div class="content">
+                <h2 class="title-anim">Biomedical</h2>
+                <!-- <a class="default-btn" href="the-campus-experience.html">Virtual Tour</a> -->
+              </div>
+            </div></div><div class="owl-item active center" style="width: 930px; margin-right: 20px;"><div class="campus-card aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+              <img src="assets/img/all-img/campus-image-2.png" alt="image">
+              <div class="content">
+                <h2 class="title-anim">Law School</h2>
+                <!-- <a class="default-btn" href="the-campus-experience.html">Virtual Tour</a> -->
+              </div>
+            </div></div><div class="owl-item active" style="width: 930px; margin-right: 20px;"><div class="campus-card aos-init aos-animate" data-aos="fade-up" data-aos-delay="300">
+              <img src="assets/img/all-img/campus-image-3.png" alt="image">
+              <div class="content">
+                <h2 class="title-anim">Humanities</h2>
+                <!-- <a class="default-btn" href="the-campus-experience.html">Virtual Tour</a> -->
+              </div>
+            </div></div><div class="owl-item cloned" style="width: 930px; margin-right: 20px;"><div class="campus-card aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+              <img src="assets/img/all-img/campus-image-1.png" alt="image">
+              <div class="content">
+                <h2 class="title-anim">Biomedical</h2>
+                <!-- <a class="default-btn" href="the-campus-experience.html">Virtual Tour</a> -->
+              </div>
+            </div></div><div class="owl-item cloned" style="width: 930px; margin-right: 20px;"><div class="campus-card aos-init aos-animate" data-aos="fade-up" data-aos-delay="200">
+              <img src="assets/img/all-img/campus-image-2.png" alt="image">
+              <div class="content">
+                <h2 class="title-anim">Law School</h2>
+                <!-- <a class="default-btn" href="the-campus-experience.html">Virtual Tour</a> -->
+              </div>
+            </div></div></div></div><div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button></div><div class="owl-dots disabled"></div></div>
+        </div>
+      </section>
+    <!-- End Campus -->
+
+  <?php include 'footer.php';?>
+  </body>
+</html>
